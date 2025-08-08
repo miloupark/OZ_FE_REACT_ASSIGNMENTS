25a398465a3de79163e6c886d6f557ea9f5b9be6
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-08 17:06:21 +0900

    style(state-props): tweak style counter

--- OZ_FE_REACT_ASSIGNMENTS_state-props/src/App.jsx
+++ OZ_FE_REACT_ASSIGNMENTS_state-props/src/App.jsx
@@ -11,8 +11,7 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <h1>⏰ 카운트 앱 생성하기 </h1>
-      <p>버튼을 누를 때 마다 버튼이 실시간으로 변경되도록 구현해 주세요.</p>
+      <h1>📟 클릭 챌린지 </h1>
       <Count count={count} />
       <button
         onClick={() => {
